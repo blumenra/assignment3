a077be3c43cccb22dff61b773604b1fbd15f2973
Alon Blumenreich <alon@mail.com>
2017-01-17 17:38:20 +0200

    Added a main function to class TPCMain in impl

--- src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
+++ src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -4,4 +4,9 @@ package bgu.spl171.net.impl.TFTPtpc;
  * Created by blumenra on 1/17/17.
  */
 public class TPCMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("bla");
+    }
 }
