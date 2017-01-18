8d35659df7b7df7c43a87f20b96665ccbbe61998
Alon Blumenreich <alon@mail.com>
2017-01-18 02:49:45 +0200

    Started to implement TFTPMain

--- src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
+++ src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -1,26 +1,26 @@
 package bgu.spl171.net.impl.TFTPtpc;
 
-import bgu.spl171.net.srv.BaseServer;
-import bgu.spl171.net.srv.BlockingConnectionHandler;
 
-import java.util.function.Supplier;
+import bgu.spl171.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl171.net.srv.Server;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Created by blumenra on 1/17/17.
  */
-public class TPCMain extends BaseServer<> {
-
-    public TPCMain(int port, Supplier protocolFactory, Supplier encdecFactory) {
-        super(port, protocolFactory, encdecFactory);
-    }
+public class TPCMain {
 
     public static void main(String[] args) {
 
-        System.out.println("bla");
+        Set<String> filesList = new ConcurrentSkipListSet<>();
+
+        Server.threadPerClient(
+                7777, //port
+                () -> new BidiMessagingProtocol<>(filesList), //protocol factory
+                TFTPEncoderDecoder::new //message encoder decoder factory
+        ).serve();
     }
 
-    @Override
-    protected void execute(BlockingConnectionHandler handler) {
-
-    }
 }
