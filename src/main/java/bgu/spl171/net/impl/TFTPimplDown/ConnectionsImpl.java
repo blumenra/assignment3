be4a537ba52c3ac659c1b1aac9ca4329dc2bc190
Alon Blumenreich <alon@mail.com>
2017-01-19 03:47:31 +0200

    More implementations of ConnectionsImpl

--- src/main/java/bgu/spl171/net/impl/TFTPimplDown/ConnectionsImpl.java
+++ src/main/java/bgu/spl171/net/impl/TFTPimplDown/ConnectionsImpl.java
@@ -4,18 +4,20 @@ import bgu.spl171.net.api.bidi.Connections;
 import bgu.spl171.net.srv.ConnectionHandler;
 
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by blumenra on 1/18/17.
  */
-public class ConnectionsImpl<T> implements Connections<T> {
+public class ConnectionsImpl<T> implements Connections<BidiMessage> {
 
-    Map<Integer, ConnectionHandler> clients = new HashMap<>();
+    private Map<Integer, ConnectionHandler> clients = new HashMap<>();
+    private Map<Integer, String> loggedInUsers = new HashMap<>();
 
     @Override
-    public boolean send(int connectionId, T msg) {
+    public boolean send(int connectionId, BidiMessage msg) {
 
         clients.get(connectionId).send(msg);
         //TODO: decide when to return what boolean
@@ -23,7 +25,7 @@ public class ConnectionsImpl<T> implements Connections<T> {
     }
 
     @Override
-    public void broadcast(T msg) {
+    public void broadcast(BidiMessage msg) {
 
         for(Integer key : clients.keySet()) {
 
@@ -34,5 +36,21 @@ public class ConnectionsImpl<T> implements Connections<T> {
     @Override
     public void disconnect(int connectionId) {
 
+        loggedInUsers.remove(connectionId);
+        clients.remove(connectionId);
+    }
+
+    public Map<Integer, ConnectionHandler> getClients() {
+
+        return clients;
+    }
+
+    public Map<Integer, String> getLoggedInUsers() {
+        return loggedInUsers;
+    }
+
+    public boolean isLoggedIn(int clientId) {
+
+        return loggedInUsers.containsKey(clientId);
     }
 }
