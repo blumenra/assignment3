9ec315b4f831c2c6da0cbb222cd6498b4201c48f
brukes <1>
2017-01-20 19:05:33 +0200

    Minor changes on tpcMain

--- src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
+++ src/main/java/bgu/spl171/net/impl/TFTPtpc/TPCMain.java
@@ -4,7 +4,12 @@ package bgu.spl171.net.impl.TFTPtpc;
 import bgu.spl171.net.impl.TFTPimplDown.*;
 import bgu.spl171.net.srv.Server;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
@@ -14,14 +19,27 @@ public class TPCMain {
 
     public static void main(String[] args) {
 
-        Set<String> filesList = new ConcurrentSkipListSet<>();
+        // REMOVE FROM HERE
 
-        //start the server
-        Server.threadPerClient(
-                7777, //port
-                () -> new BidiServerProtocolImpl<>(filesList), //protocol factory
-                BidiEncDecImpl::new //message encoder decoder factory
-        ).serve();
+        File file = new File("Files/removeMe.txt");
+        System.out.println(file.exists());
+
+        // REMOVE UP TO HERE
+
+
+
+
+
+
+
+//        Map<String, Boolean> filesList = new ConcurrentHashMap<>();
+//
+//        //start the server
+//        Server.threadPerClient(
+//                7777, //port
+//                () -> new BidiServerProtocolImpl(filesList), //protocol factory
+//                BidiEncDecImpl::new //message encoder decoder factory
+//        ).serve();
     }
 
 }
