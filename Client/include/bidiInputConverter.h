e17273651fa79d784f823c93f5089f1bf9f16707
Alon Blumenreich <alon@mail.com>
2017-01-21 16:33:32 +0200

    Created BidiInputCOnverter class which will conver input from the client into bidiMessage

---
+++ Client/include/bidiInputConverter.h
@@ -0,0 +1,21 @@
+
+#ifndef BIDI_CONVERTER_H
+#define BIDI_CONVERTER_H
+
+#include <string>
+#include <iostream>
+
+#endif //CLIENT_BIDIMESSAGE_H
+
+using namespace std;
+
+class bidiInputConverter {
+
+private:
+
+
+
+public:
+
+
+};
